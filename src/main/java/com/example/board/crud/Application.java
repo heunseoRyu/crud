73d9d1faa3dad317e0daeb7bb9bb5d89d9321d23
heunseoRyu <120763372+heunseoRyu@users.noreply.github.com>
2023-10-16 20:07:51 +0900

    2t

--- src/main/java/com/example/board/crud/Application.java
+++ src/main/java/com/example/board/crud/Application.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 // 중요! 실수로 yml을 지운다면 형벌이 내려짐.
 @SpringBootApplication
 @EnableJpaAuditing // jpa auditing 활성화
+// 2트
 public class Application {
 
 	public static void main(String[] args) {
